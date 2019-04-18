dd3dcb8f92951431a7c6efee2aa0a0b515e84119
Ghiles <ghiles.cherfaoui@soat.fr>
2019-04-18 15:21:56 +0200

    adding fake data

---
+++ src/main/scala/giveaways/FakeData.scala
@@ -0,0 +1,23 @@
+package giveaways
+
+import akka.Done
+
+object FakeData {
+
+  final case class Item(name: String, id: Long)
+
+  final case class Order(items: List[Item])
+
+  var orders: List[Item] = List(Item("item1", 1), Item("item2", 2))
+
+
+  def fetchItem(itemId: Long): Option[Item] = orders.find(o => o.id == itemId)
+
+  def saveOrder(order: Order): Done = {
+    orders = order match {
+      case Order(items) => items ::: orders
+      case _ => orders
+    }
+    Done
+  }
+}
