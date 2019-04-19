51abbb5da2b8b0d7de88dbdab7931a3f95406252
Ghiles <ghiles.cherfaoui@soat.fr>
2019-04-19 15:18:03 +0200

    refactoring + adding surveys and giveaways

--- src/main/scala/giveaways/FakeData.scala
+++ src/main/scala/giveaways/FakeData.scala
@@ -5,13 +5,18 @@ import akka.Done
 object FakeData {
 
   final case class Item(name: String, id: Long, amount: Double)
+
   final case class Order(items: List[Item])
+
   final case class User(id: Int, name: String, subscribed: Boolean, blocked: Boolean)
+
   final case class Survey(id: Int, question: String, option1: String, option2: String, vote1: Int, vote2: Int, participants: List[User])
+
   final case class Tip(id: Int, name: String, amount: Double)
+
   final case class GiveAway(id: Int, event: String, amount: Double, participants: List[User])
 
-  /**********************DATA********************/
+  /** ********************DATA ********************/
 
   var users: List[User] = List(
     User(1, "jesus", true, true),
@@ -25,9 +30,9 @@ object FakeData {
   var tips: List[Tip] = List(
     Tip(1, "jesus", 10),
     Tip(2, "jawad", 1),
-    Tip(3, "Sarah", 3),
-    Tip(4, "Jean", 5),
-    Tip(5, "Coralie", 20)
+    Tip(3, "lvmh", 3),
+    Tip(4, "jeankevin", 5),
+    Tip(5, "jeanmouloud", 20)
   )
   var giveaways: List[GiveAway] = List(
     GiveAway(1, "Event1", 1000, List(
@@ -36,6 +41,7 @@ object FakeData {
       User(4, "lvmh", false, false),
       User(5, "jeankevin", false, false),
       User(6, "jeanmouloud", false, false),
+
     )),
     GiveAway(2, "Event4", 1000, List[User]())
   )
@@ -48,9 +54,9 @@ object FakeData {
       10,
       20,
       List(
-        User(7, "Ahmed", false, false),
-        User(8, "Beatrice", false, false),
-        User(9, "Sarah", false, false)
+        User(3, "Ahmed", false, false),
+        User(5, "Beatrice", false, false),
+        User(1, "Sarah", false, false)
       )),
     Survey(
       2,
@@ -60,48 +66,15 @@ object FakeData {
       10,
       20,
       List(
-        User(3, "Benoit", false, false),
-        User(8, "Beatrice", false, false),
-        User(9, "Sarah", false, false)
-      )),
-    Survey(
-      3,
-      "Q3",
-      "OPT1",
-      "OPT2",
-      10,
-      20,
-      List(
-        User(1, "Jean", false, false),
-        User(2, "Sebastien", false, false),
-        User(9, "Sarah", false, false)
+        User(2, "jawad", false, false),
+        User(4, "lvmh", false, false),
       ))
   )
 
-  var idCount = 4
-  var orders: List[Item] = List(Item("Dieu", 1, 10), Item("Jesus", 2, 20), Item("michelJackson", 3, 30), Item("jawad", 3, 300))
-  private var blackListed: Set[String] = Set()
-  private var followers: Set[String] = Set()
+  /** ********************************************/
 
 
-  def fetchItembById(itemId: Long): Option[Item] = orders.find(o => o.id == itemId)
-
-  def fetchItembByName(itemName: String): Option[Item] = orders.find(o => o.name == itemName)
-
-  def saveItem(item: Item): Done = {
-    orders = item match {
-      case i: Item => List(i) ::: orders
-      case _ => orders
-    }
-    Done
-  }
-
-
-
-
-
-
-  /********************Tips******************/
+  /** ******************Tips ******************/
 
   def tip(name: String, amount: Double): List[Tip] = {
     if (users.exists(_.name == name))
@@ -111,15 +84,15 @@ object FakeData {
   }
 
   def deleteTip(id: Int): Unit = {
-    tips= tips.filter(_.id!=id)
+    tips = tips.filter(_.id != id)
   }
 
   def tipSum(): Option[Double] = {
     Some(tips.map(_.amount).sum)
   }
 
-  def getTipByUser(name: String):Double = {
-    tips.filter(_.name==name).map(_.amount).sum
+  def getTipByUser(name: String): Double = {
+    tips.filter(_.name == name).map(_.amount).sum
   }
 
   def listOfDonators(): List[String] = {
@@ -130,31 +103,51 @@ object FakeData {
     Some(tips.map(_.amount).sum)
   }
 
-  /**********************/
+  /** *********************SUBS *********************/
 
+  def getSubs() = {
+    users.filter(_.subscribed).map(_.name)
+  }
 
+  /** *****************GiveAways ********************/
 
+  def newGiveaway(id: Int, event: String, cashPrize: Double, participants: List[User]): List[GiveAway] = {
+    giveaways :+ GiveAway(id, event, cashPrize, participants)
+  }
 
+  def removeGiveaway(id: Int): Unit = {
+    giveaways = giveaways.filter(_.id != id)
+  }
 
-  def saveOrder(order: Order): Done = {
-    orders = order match {
-      case Order(items) => items ::: orders
-      case _ => orders
+  private def subToGiveaway(id: Int, user: User) = {
+    giveaways.find(_.id == id) match {
+      case Some(g) =>
+        val ga = g
+        val l = g.participants ++ List(user)
+        removeGiveaway(g.id)
+        giveaways = newGiveaway(ga.id, ga.event, ga.amount, l)
+      case _ => giveaways
     }
-    Done
+  }
+
+  def getWinner(id: Int): User = {
+    val r = scala.util.Random
+    val list = giveaways.filter(_.id == id)(0)
+    list.participants(r.nextInt(list.participants.length))
   }
 
 
-  /*
+  /** *********************Orders *****************/
 
- */
 
-  /* USERS */
+
+  /** **********************Users *******************/
   def blockUser(id: Int): Unit = {
     users.filter(_.id == id)
   }
 
-  /* SURVEYS */
+  /** ********************Surveys ******************/
+
   def newSurvey(question: String, option1: String, option2: String): Unit = {
     surveys :+ Survey(surveys.length + 1, question, option1, option2, 0, 0, List())
   }
